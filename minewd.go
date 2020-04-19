13bc8bda86c62fffe5549a54d1f145c474a4b08d
Ruben de Groot <ruben@hacktor.com>
2020-04-19 10:13:28 +0200

    refactor

--- minewd.go
+++ minewd.go
@@ -22,6 +22,7 @@ type dbRecord struct {
 }
 
 type packet struct {
+	content []byte
 	datal   uint32
 	remAddr string
 	reqLen  int
@@ -41,7 +42,7 @@ func main() {
 	}
 
 	// start database goroutine
-	c := make(chan dbRecord, 32)
+	c := make(chan dbRecord, 64)
 	go database(cfg, c)
 
 	// start listening
@@ -116,13 +117,13 @@ func handleConn(conn net.Conn, c chan dbRecord) {
 	defer conn.Close()
 
 	// Make a buffer to hold incoming data.
-	buf := make([]byte, 4096)
 	var p packet
+	p.content = make([]byte, 16384)
 	var err error
 
 	// Read the incoming connection into the buffer.
 	p.remAddr = conn.RemoteAddr().String()
-	p.reqLen, err = conn.Read(buf)
+	p.reqLen, err = conn.Read(p.content)
 	if err != nil {
 		log.Println(p.remAddr, "closed connection:", err.Error())
 		return
@@ -130,25 +131,28 @@ func handleConn(conn net.Conn, c chan dbRecord) {
 	// log.Println("From", remAddr, ":", buf[:reqLen])
 
 	// Analyze packet
-	p.analyzeBLE(buf, c)
+	p.analyzeBLE(c)
 
 	// Send a response back
-	_, err = conn.Write([]byte("Message received: " + string(buf)))
+	_, err = conn.Write([]byte("Message received: " + string(p.content)))
 	if err != nil {
 		log.Println("Error wrinting to", p.remAddr, ":", err.Error())
 	}
 }
 
-func (p packet) analyzeBLE(buf []byte, c chan dbRecord) {
+func (p packet) analyzeBLE(c chan dbRecord) {
 
-	if buf[0] == 187 && buf[p.reqLen-1] == 221 {
+	if p.content[0] == 187 && p.content[p.reqLen-1] == 221 && p.reqLen > 22 {
 
-		p.datal = binary.BigEndian.Uint32(buf[2:6])
-		p.boxID = hex.EncodeToString(buf[6:12])
-		p.nrBLE = binary.BigEndian.Uint16(buf[12:14])
+		p.datal = binary.BigEndian.Uint32(p.content[2:6])
+		p.boxID = hex.EncodeToString(p.content[6:12])
+		p.nrBLE = binary.BigEndian.Uint16(p.content[12:14])
+	} else {
+		log.Println("Ignoring corrupted packet")
+		return
 	}
 
-	ble := buf[14 : p.reqLen-1]
+	ble := p.content[14:]
 
 	for i := p.nrBLE; i > 0; i-- {
 
@@ -157,23 +161,35 @@ func (p packet) analyzeBLE(buf []byte, c chan dbRecord) {
 
 		tagID := hex.EncodeToString(ble[0:6])
 		nrBLEdata := binary.BigEndian.Uint16(ble[6:8])
-		ble = ble[8:]
+		if len(ble) > 8 {
+			ble = ble[8:]
+		} else {
+			return
+		}
 		for d = 0; d < nrBLEdata; d++ {
 
 			rawLen := int(ble[0])
+			if len(ble) < rawLen+2 {
+				log.Println("Packet length exceeded")
+				return
+			}
 			var db dbRecord
 			db.dateTime = tnow
 			db.boxID = p.boxID
 			db.tagID = tagID
 			if rawLen != 0 {
-				db.data = hex.EncodeToString(buf[1 : 1+rawLen])
+				db.data = hex.EncodeToString(ble[1 : 1+rawLen])
 			}
 			db.rssi = int8(ble[rawLen+2])
 
 			// send dbRecord to database through channel
 			c <- db
 
-			ble = ble[rawLen+3:]
+			if len(ble) > rawLen+3 {
+				ble = ble[rawLen+3:]
+			} else {
+				return
+			}
 		}
 	}
 }
