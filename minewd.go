77575aed560b51b1899dd71ac31490a054b91308
Ruben de Groot <ruben@hacktor.com>
2020-04-26 12:21:07 +0200

    Split of handler of binary packets

--- minewd.go
+++ minewd.go
@@ -2,8 +2,6 @@ package main
 
 import (
     "database/sql"
-    "encoding/binary"
-    "encoding/hex"
     "log"
     "net"
     "os"
@@ -19,15 +17,6 @@ type dbRecord struct {
     boxID    string
     rssi     int8
     data     string
-}
-
-type packet struct {
-    content []byte
-    datal   uint32
-    remAddr string
-    reqLen  int
-    boxID   string
-    nrBLE   uint16
 }
 
 // global variables to share between main and the handlers
@@ -116,88 +105,6 @@ func database(cfg *ini.File) {
                 }
             }
             log.Printf("%+v\n", r)
-        }
-    }
-}
-
-func handleConn(conn net.Conn) {
-
-    defer conn.Close()
-
-    // Make a buffer to hold incoming data.
-    var p packet
-    p.content = make([]byte, 16384)
-    var err error
-
-    // Read the incoming connection into the buffer.
-    p.remAddr = conn.RemoteAddr().String()
-    p.reqLen, err = conn.Read(p.content)
-    if err != nil {
-        log.Println(p.remAddr, "closed connection:", err.Error())
-        return
-    }
-    // log.Println("From", remAddr, ":", buf[:reqLen])
-
-    // Analyze packet
-    p.analyzeBLE()
-
-    // Send a response back
-    _, err = conn.Write([]byte("Message received: " + string(p.content)))
-    if err != nil {
-        log.Println("Error wrinting to", p.remAddr, ":", err.Error())
-    }
-}
-
-func (p packet) analyzeBLE() {
-
-    if p.content[0] == 187 && p.content[p.reqLen-1] == 221 && p.reqLen > 22 {
-
-        p.datal = binary.BigEndian.Uint32(p.content[2:6])
-        p.boxID = hex.EncodeToString(p.content[6:12])
-        p.nrBLE = binary.BigEndian.Uint16(p.content[12:14])
-    } else {
-        log.Println("Ignoring corrupted packet")
-        return
-    }
-
-    ble := p.content[14:]
-
-    for i := p.nrBLE; i > 0; i-- {
-
-        var d uint16
-        tnow := sqltime(time.Now().Unix())
-
-        tagID := hex.EncodeToString(ble[0:6])
-        nrBLEdata := binary.BigEndian.Uint16(ble[6:8])
-        if len(ble) > 8 {
-            ble = ble[8:]
-        } else {
-            return
-        }
-        for d = 0; d < nrBLEdata; d++ {
-
-            rawLen := int(ble[0])
-            if len(ble) < rawLen+2 {
-                log.Println("Packet length exceeded")
-                return
-            }
-            var db dbRecord
-            db.dateTime = tnow
-            db.boxID = p.boxID
-            db.tagID = tagID
-            if rawLen != 0 {
-                db.data = hex.EncodeToString(ble[1 : 1+rawLen])
-            }
-            db.rssi = int8(ble[rawLen+2])
-
-            // send dbRecord to database through channel
-            c <- db
-
-            if len(ble) > rawLen+3 {
-                ble = ble[rawLen+3:]
-            } else {
-                return
-            }
         }
     }
 }
