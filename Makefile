7bd21fe4aeba7afb57df5d5c9372baebed3eeb6c
Ruben de Groot <ruben@hacktor.com>
2020-04-26 13:29:10 +0200

    Start work json parser

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
-all: build-all-bin
+all: build-all-bin build-all-json
 
 build-all-bin: build-linux-bin build-windows-bin
+
+build-all-json: build-linux-json build-windows-json
 
 build-linux-bin:
 	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o minewd-bin minewd.go binpacket.go
 
 build-windows-bin:
 	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o minewd-bin.exe minewd.go binpacket.go
+
+build-linux-json:
+	CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o minewd-json minewd.go jsonpacket.go
+
+build-windows-json:
+	CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o minewd-json.exe minewd.go jsonpacket.go
+
---
+++ jsonpacket.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "encoding/json"
+    "encoding/hex"
+    "log"
+    "net"
+    "time"
+
+    _ "github.com/go-sql-driver/mysql"
+)
+
+type packet struct {
+    content []byte
+    remAddr string
+    reqLen  int
+    boxID   string
+}
+
+func handleConn(conn net.Conn) {
+
+    defer conn.Close()
+
+    // Make a buffer to hold incoming data.
+    var p packet
+    p.content = make([]byte, 16384)
+    var err error
+
+    // Read the incoming connection into the buffer.
+    p.remAddr = conn.RemoteAddr().String()
+    p.reqLen, err = conn.Read(p.content)
+    if err != nil {
+        log.Println(p.remAddr, "closed connection:", err.Error())
+        return
+    }
+    // log.Println("From", remAddr, ":", buf[:reqLen])
+
+    // Analyze packet
+    p.analyzeBLE()
+
+    // Send a response back
+    _, err = conn.Write([]byte("Message received: " + string(p.content)))
+    if err != nil {
+        log.Println("Error wrinting to", p.remAddr, ":", err.Error())
+    }
+}
+
+func (p packet) analyzeBLE() {
+
+    // p.content is not quite json yet, so this will fail
+    var tags map[string]interface{}
+    json.Unmarshal([]byte(byteValue), &tags)
+}
+
